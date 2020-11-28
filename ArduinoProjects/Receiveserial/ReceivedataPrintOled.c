125d76f01e8bcbd4d5aaad67f49c4209f7022a42
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-11-28 18:52:35 +0200

    Receive char and print it on oled screen

---
+++ ArduinoProjects/Receiveserial/ReceivedataPrintOled.c
@@ -0,0 +1,77 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+char receivedChar;
+boolaw newData = false;
+const int D12 = 12;
+
+void testdrawchar(char print);
+
+void setup() {
+    
+    Serial.begin(9600);
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Don't proceed, loop forever
+    }
+    Serial.println("<Arduino is ready>");
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(D12, OUTPUT);
+}
+
+void loop() {
+    recvOneChar();
+    showNewData();
+}
+
+void testdrawchar(char print) {
+    display.clearDisplay();
+    //Size 3 approx 7 letters in one line
+    display.setTextSize(2);      // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.setCursor(0, 0);     // Start at top-left corner
+    display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+    display.write(print);
+    // Show the display buffer on the screen. You MUST call display() after
+    // drawing commands to make them visible on screen!
+    display.display();
+    delay(10);
+    // display.display() is NOT necessary after every single drawing command,
+    // unless that's what you want...rather, you can batch up a bunch of
+    // drawing operations and then update the screen all at once by calling
+    // display.display().
+}
+
+void recvOneChar() {
+    if (Serial.available() > 0) {
+        receivedChar = Serial.read();
+        newData = true;
+    }
+}
+
+void showNewData() {
+    if (newData == true) {
+        Serial.print("Received: ");
+        Serial.println(receivedChar);
+        testdrawchar(receivedChar);
+        digitalWrite(LED_BUILTIN, HIGH); 
+        digitalWrite(D12, HIGH);  
+        delay(10);                       
+        digitalWrite(LED_BUILTIN, LOW); 
+        digitalWrite(D12, LOW);     
+        delay(10);        
+        newData = false;
+    }
+}
