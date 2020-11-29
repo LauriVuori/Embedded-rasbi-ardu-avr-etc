0634a2419dafcffb21da4a96e2c51887707fb898
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-11-29 15:56:07 +0200

    update

--- Avr/Attiny2313A/TestingSoft/Chiptesting/main.c
+++ Avr/Attiny2313A/TestingSoft/Chiptesting/main.c
@@ -15,22 +15,64 @@
 // typedef unsigned long long int uint64_t
 
 
-int main (void)
-{
-	DDRB=0xFF; //suuntarekisteri ja suunta ulos
-	PORTB=0xff; //B-port 1111 1111 leds off
-	DDRD=~((1<<PD2)|(1<<PD3)); //suuntarekisteri , bitti siirto
-	PORTD=0xff; //D-porttiin
+int main (void){
+	// DDRB=0xFF; //suuntarekisteri ja suunta ulos
+	// PORTB=0xff; //B-port 1111 1111 leds off
+	// DDRD=~((1<<PD2)|(1<<PD3)); //suuntarekisteri , bitti siirto
+	// PORTD=0xff; //D-porttiin
 
 
-	while(1){
-		while(~PIND & (1<<PD2)) {
-			PORTB =~2;
+	// while(1){
+	// 	while(~PIND & (1<<PD2)) {
+	// 		PORTB =~15;
+	// 	}
+	// 	while(~PIND & (1<<PD3)){
+    //         PORTB =2;
+	// 	}
+	// 	PORTB=0xff;
+	// }
+	    
+		
+	init_pwm();
+	DDRD=~((1<<PD2)|(1<<PD3));
+	PORTD=0xff;
+	int dimmer = 0;
+    while(1){
+		if(~PIND & (1<<PD2)) {  
+
+			dimmer = 250;
+			// _delay_ms(10);
+
+			// _delay_ms(100);
+
 		}
-		while(~PIND & (1<<PD3)){
-            PORTB =~1;
-		}
-		PORTB=0xff;
+		
+		// if(~PIND & (1<<PD3)){ // 
+		// 	dimmer = 0;
+		// 	// _delay_ms(100);
+
+		// }
+		// for (int i = 0; i < 250; i++){
+		// 	OCR0A  = i;
+		// 	_delay_ms(10);
+		// }
+		// for (int i = 10; i < 250; i++){
+		// 	DDRB |= (1<<PB3);
+		// 	OCR0A  = i;
+		// 	_delay_ms(10);
+		// }
+		// DDRB |= (0<<PB3);
+		// for (int i = 250; i > 10; i--){
+		// 	DDRB |= (1<<PB4);
+		// 	OCR0A  = i;
+		// 	_delay_ms(10);
+		// }
+		// DDRB |= (0<<PB4);
+	OCR0A  = dimmer;
 	}
 }
 
+// for (int i = 0; i < 250; i++){
+// 	OCR0A  = i;
+// 	_delay_ms(10);
+// }
--- Avr/Attiny2313A/TestingSoft/Chiptesting/test.c
+++ Avr/Attiny2313A/TestingSoft/Chiptesting/test.c
@@ -12,17 +12,29 @@
 // (1<<PD2)		0000 0100
 //	&			0000 0100
 // tulos		0000 0100 tosi koska poikkeaa nollasta
+// PD2 upper button
+// PD3 lower button
+void init_leds(){
+	DDRB=0xFF; //suuntarekisteri ja suunta ulos
+	PORTB=0xff; //B-port 1111 1111 leds off
+	DDRD=~((1<<PD2)|(1<<PD3)); //suuntarekisteri , bitti siirto
+	PORTD=0xff; //D-porttiin
+
+}
+void init_pwm(){
+	DDRB   |= (1 << PB2);                   // PWM output on PB2
+	TCCR0A = (1 << COM0A1) | (1 << WGM00);  // phase correct PWM mode
+	OCR0A  = 0x10;                          // initial PWM pulse width
+		
+	TCCR0B = (1 << CS01);   // clock source = CLK/8, start PWM
+}
 
 
-
-
-void test();
 
 void test(){
 	int a = 0;
 	int suunta = 0;
 		while(~PIND & (1<<PD2)) {
-
 			_delay_ms(60);
 			PORTB=~(0x07 << a); // looginen alatila on ledi p??ll? 1111 1110 1111 1100
 			if ((a <= 5) && (suunta == 0)){
