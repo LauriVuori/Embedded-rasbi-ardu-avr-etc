37d1b915776b5c3fc88baa9e6c4a01da7261025e
LauriVuori <Laurivuori.lv@gmail.com>
2020-12-27 00:49:03 +0200

    update

--- ArduinoProjects/Receiveserial/RecWithoutGlobals.c
+++ ArduinoProjects/Receiveserial/RecWithoutGlobals.c
@@ -5,29 +5,52 @@
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void testdrawchar(char print[]);
+bool stringComplete = false;
+char test[15];
+int b = 0;
+
 
 void setup() {
-    Serial.begin(9600);
-
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-        Serial.println(F("SSD1306 allocation failed"));
-        for(;;); // Don't proceed, loop forever
-    }
+  // initialize serial:
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 }
 
 void loop() {
-
-        testdrawchar((char*)"terve");
-
-
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    stringComplete = false;
+    Serial.println("STRING::");
+    testdrawchar(test);
+    b = 0;
+  }
 }
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    test[b] = inChar;
+    b++;
+    if (inChar == '\n') {
+      test[b] = '\0';
+      stringComplete = true;
+    }
+  }
+}
+
 
 void testdrawchar(char print[]) {
     display.clearDisplay();
