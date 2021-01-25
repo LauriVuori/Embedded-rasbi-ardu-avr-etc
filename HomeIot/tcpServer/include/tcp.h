a4748e15c5984f939e5a259d6bf0834b17cfeb60
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-01-25 17:14:07 +0200

    added header file

--- HomeIot/tcpServer/include/tcp.h
+++ HomeIot/tcpServer/include/tcp.h
@@ -1,3 +1,44 @@
-void func(int sockfd);
+#define MAX 80 
+#define PORT 8080
+#define MAXCONNECTIONS 1
+#define SA struct sockaddr
+
+/*!
+ * @enum boolean
+ * @brief boolean variable
+ * @param false 0
+ * @param true 1
+*/ 
+enum boolean {
+    false = 0, 
+    true = 1
+};
+
+/* Global structures */
+
+/*!
+ * @struct tcpErrors
+ * @brief flag errors
+ * @param enum boolean zeroBuffer
+*/ 
+struct tcpErrors {
+    enum boolean zeroBuffer;
+};
+
+/*!
+ * @struct tcpOptions
+ * @brief options for tcp
+ * @param enum boolean sendDataBack
+*/ 
+struct tcpOptions{
+    enum boolean sendDataBack;
+};
+
+/*-------------------------------------------------------------------*
+*    FUNCTION PROTOTYPES                                             *
+*--------------------------------------------------------------------*/
+void func(int * sockfd);
+void receiveData(int * sockfd, struct tcpErrors * error, struct tcpOptions * options);
+void sendData(int * sockfd, char (*data)[MAX]);
 void initSocket(struct sockaddr_in * servAddr, int * sockfd);
 void acceptClient(struct sockaddr_in * servAddr, int * sockfd, int * accClient);
