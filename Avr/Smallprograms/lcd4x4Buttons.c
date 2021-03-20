a3240ee21f47dd9590a8e7086d70cc1a7f6ca25f
LauriVuori <Laurivuori.lv@gmail.com>
2021-03-20 18:39:17 +0200

    Working version

--- Avr/Smallprograms/lcd4x4Buttons.c
+++ Avr/Smallprograms/lcd4x4Buttons.c
@@ -5,12 +5,11 @@
 
 #define ROWMAX 2
 #define COLMAX 16
-
 void wait(uint16_t time);
-void drawing(uint8_t * button, uint8_t * currentRow, uint8_t * currentCol);
+void drawing(uint8_t button, uint8_t * currentRow, uint8_t * currentCol);
 unsigned char luenappi();
 unsigned char test();
-
+void printBinary(uint8_t number, uint8_t * currentRow, uint8_t * currentCol);
 int main(void) {
 	DDRD = 0xf0;
 	PORTD = 0x0f;
@@ -27,7 +26,8 @@ int main(void) {
 	while(1) {
 		wait (10);
 		button = luenappi();
-		// drawing(&button, &currentRow, &currentCol);
+		wait(20);
+		drawing(button, &currentRow, &currentCol);
 		// if (currentCol > 15) {
 		// 	currentCol = 0;
 		// }
@@ -42,33 +42,48 @@ int main(void) {
 		wait (100);
 	}
 }
-void drawing(uint8_t * button, uint8_t * currentRow, uint8_t * currentCol) {
+void printBinary(uint8_t number, uint8_t * currentRow, uint8_t * currentCol) {
+	int i, k;
+	for (i = 16; i >= 0; i--) {
+		k = number >> i;
+		if (k & 1) {
+			drawing(k, currentRow, currentCol);
+		}
+		else {
+			drawing(k, currentRow, currentCol);
+		}
+	}
+}
+
+
+void drawing(uint8_t button, uint8_t * currentRow, uint8_t * currentCol) {
 
 	if (*currentRow > 15 ) {
 		*currentRow = 0;
 	}
-	if (*button == 16) {
+	if (button == 16) {
 		*currentRow = 0;
 		*currentCol = 0;
 		LCD_Clear();
 		LCD_SetCursorXY(*currentRow, *currentCol);
 	}
-	if (*button == 15) {
+	if (button == 15) {
 		if(*currentCol > 0) {
 			*currentCol -= 1;
 		}
 		else {
 			*currentCol += 1;
 		}
+		LCD_SetCursorXY(*currentRow, *currentCol);
 	}
-	else if ((*button != 0) && (*button != 16) && (*button != 15)) {
-		if (*button < 10) {
-			LCD_WriteUINT(*button);
+	else if ((button != 0) && (button != 16) && (button != 15)) {
+		if (button < 10) {
+			LCD_WriteUINT(button);
 			*currentRow += 1;
 			LCD_SetCursorXY(*currentRow, *currentCol);
 		}
-		else if (*button > 10) {
-			LCD_WriteUINT(*button);
+		else if (button >= 10) {
+			LCD_WriteUINT(button);
 			*currentRow += 2;
 			LCD_SetCursorXY(*currentRow, *currentCol);
 		}
@@ -79,17 +94,16 @@ void drawing(uint8_t * button, uint8_t * currentRow, uint8_t * currentCol) {
 unsigned char luenappi() {
 	unsigned char a=0x00;
 
-	PORTD = ~0x10;
+	PORTD = ~0x10; // 1110 1111
 	wait(10);
 	if (~PIND & 0x01) a = 1;
 	if (~PIND & 0x02) a = 2;
 	if (~PIND & 0x04) a = 3;
 	if (~PIND & 0x08) a = 4;
-	wait(10);
+
 	PORTD = 0xff;
 
-	wait(10);
-	PORTD = ~0x20;
+	PORTD = ~0x20; // 1101 1111
 	wait(10);
 	if (~PIND & 0x01) a = 5;
 	if (~PIND & 0x02) a = 6;
@@ -98,23 +112,27 @@ unsigned char luenappi() {
 
 	PORTD = 0xff;
 
-	wait(100);
-	PORTD = ~0x40 ;
-	wait (10);
+	PORTD = ~0x40 ; // 1011 1111
+	wait(10);
 	if (~PIND & 0x01) a = 9;
 	if (~PIND & 0x02) a = 10;
 	if (~PIND & 0x04) a = 11;
 	if (~PIND & 0x08) a = 12;
-	wait(10);
 	PORTD = 0xff;
 
-	PORTD = ~0x80 ;
-	wait (10);
-	if (~PIND & 0x01) a = 13;
-	if (~PIND & 0x02) a = 14;
-	if (~PIND & 0x04) a = 15;
-	if (~PIND & 0x08) a = 16;
-	wait(10);
+	PORTD = ~0x80 ;// 0111 1111
+	if (~PIND & 0x01) {while(~PIND & 0x01){a = 13;}}
+	if (~PIND & 0x02){ while(~PIND & 0x02){a = 14;}}
+	if (~PIND & 0x04) {
+		while (~PIND & 0x04) {
+			a = 15;
+		}
+	}
+	if (~PIND & 0x08){
+		while (~PIND & 0x08){
+			a = 16;
+		}
+	} 
 	return (a);
 }
 // *** Primitive wait() ***
