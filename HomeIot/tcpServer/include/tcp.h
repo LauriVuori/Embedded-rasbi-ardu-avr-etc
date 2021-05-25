69ce2d35bc5a4c37eb4a4145fecdda58d46dde9c
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-05-25 21:11:19 +0300

    update

--- HomeIot/tcpServer/include/tcp.h
+++ HomeIot/tcpServer/include/tcp.h
@@ -1,23 +1,3 @@
-#include <stdio.h> 
-#include <netdb.h> 
-#include <netinet/in.h> 
-#include <stdlib.h> 
-#include <signal.h>
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h>
-#include <pthread.h>
-
-
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define RESET "\x1B[0m"
-
 #define MAX 80 
 #define PORT 8080
 #define MAXCONNECTIONS 1
@@ -60,5 +40,5 @@ struct tcpOptions{
 void func(int * sockfd);
 void receiveData(int * sockfd, struct tcpErrors * error, struct tcpOptions * options);
 void sendData(int * sockfd, char (*data)[MAX]);
-int initSocket(struct sockaddr_in * servAddr, int sockfd);
-int acceptClient(struct sockaddr_in * client, int * sockfd, int acceptClient);
+void initSocket(struct sockaddr_in * servAddr, int * sockfd);
+void acceptClient(struct sockaddr_in * servAddr, int * sockfd, int * accClient);
