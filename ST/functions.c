43e68df4dc59140903b4bedde566665f3ca89903
LauriVuori <Laurivuori.lv@gmail.com>
2021-09-15 11:37:33 +0300

    added uint to char array for printing

--- ST/functions.c
+++ ST/functions.c
@@ -48,4 +48,63 @@ void format_to_binary_usart2(uint32_t n) {
     USART2_write_string(binary_form);
 }
 
+void uint8_to_char_array(uint8_t uInteger, uint8_t * dest_array) {
+    // needs atleast [6] array
+	uint8_t* ptr_to_buffer;
+	uint8_t int_buffer [5];
 
+	ptr_to_buffer = &int_buffer[4];        // Point to buffer end
+    *ptr_to_buffer = 0;
+	do {
+        // make characters, (uInteger % 10) = 5, 5 + '0' = 53=='5'
+        *--ptr_to_buffer = (uInteger % 10) + '0';
+    }while (uInteger /= 10 );
+	while (*ptr_to_buffer) {
+        *dest_array = *ptr_to_buffer;
+        dest_array++;
+        ptr_to_buffer++;
+	}
+    // end char
+    *dest_array = '\0';
+}
+
+void uint16_to_char_array(uint16_t uInteger, uint8_t * dest_array) {
+	// needs atleast [6] array
+	uint8_t* ptr_to_buffer;
+	uint8_t int_buffer [11];
+
+	ptr_to_buffer = &int_buffer[10];        // Point to buffer end
+	*ptr_to_buffer = 0;
+	do {
+		// make characters, (uInteger % 10) = 5, 5 + '0' = 53=='5'
+		*--ptr_to_buffer = (uInteger % 10) + '0';
+	}while (uInteger /= 10 );
+
+	while (*ptr_to_buffer) {
+		*dest_array = *ptr_to_buffer;
+		dest_array++;
+		ptr_to_buffer++;
+	}
+	// end char
+	*dest_array = '\0';
+}
+void uint32_to_char_array(uint32_t uInteger, uint8_t * dest_array) {
+    // needs atleast [6] array
+	uint8_t* ptr_to_buffer;
+	uint8_t int_buffer [12];
+
+	ptr_to_buffer = &int_buffer[11];        // Point to buffer end
+    *ptr_to_buffer = 0;
+    do {
+        // make characters, (uInteger % 10) = 5, 5 + '0' = 53=='5'
+        *--ptr_to_buffer = (uInteger % 10) + '0';
+    }while (uInteger /= 10 );
+
+	while (*ptr_to_buffer) {
+        *dest_array = *ptr_to_buffer;
+        dest_array++;
+        ptr_to_buffer++;
+	}
+    // end char
+    *dest_array = '\0';
+}
--- ST/functions.h
+++ ST/functions.h
@@ -2,3 +2,7 @@
 #include "../main.h"
 void format_to_binary(uint32_t n);
 void format_to_binary_usart2(uint32_t n);
+
+void uint16_to_char_array(uint16_t uInteger, uint8_t * dest_array);
+void uint32_to_char_array(uint32_t uInteger, uint8_t * dest_array);
+void uint8_to_char_array(uint8_t uInteger, uint8_t * dest_array);
